c9a2c377b90b2e48c8d47e532b5180dac568978c
izsl <iamppz@hotmail.com>
2019-04-01 00:41:20 +0800

    静态页面

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,14 +1,130 @@
 <template>
-    <p>
-        Hello!
-    </p>
+    <el-row :gutter="20">
+        <el-col :span="6">
+            <el-input placeholder="输入关键字进行过滤" v-model="filterText">
+            </el-input>
+            <el-tree class="filter-tree" :data="data2" :props="defaultProps" default-expand-all
+                     :filter-node-method="filterNode" ref="tree2">
+            </el-tree>
+        </el-col>
+        <el-col :span="18">
+            <el-table :data="tableData" style="width: 100%">
+                <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
+                <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+                <el-table-column prop="province" label="省份" width="120"></el-table-column>
+                <el-table-column prop="city" label="市区" width="120"></el-table-column>
+                <el-table-column prop="address" label="地址" width="300"></el-table-column>
+                <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+                <el-table-column fixed="right" label="操作" width="100">
+                    <template slot-scope="scope">
+                        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                        <el-button type="text" size="small">编辑</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage4"
+                    :page-sizes="[100, 200, 300, 400]"
+                    :page-size="100"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="400">
+            </el-pagination>
+        </el-col>
+    </el-row>
 </template>
 <script>
     export default {
-        data: function () {
-            return {}
+        watch: {
+            filterText(val) {
+                this.$refs.tree2.filter(val);
+            }
         },
-        methods: {}
+
+        methods: {
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.label.indexOf(value) !== -1;
+            },
+            handleClick(row) {
+                console.log(row);
+            }
+        },
+
+        data() {
+            return {
+                filterText: '',
+                data2: [{
+                    id: 1,
+                    label: '一级 1',
+                    children: [{
+                        id: 4,
+                        label: '二级 1-1',
+                        children: [{
+                            id: 9,
+                            label: '三级 1-1-1'
+                        }, {
+                            id: 10,
+                            label: '三级 1-1-2'
+                        }]
+                    }]
+                }, {
+                    id: 2,
+                    label: '一级 2',
+                    children: [{
+                        id: 5,
+                        label: '二级 2-1'
+                    }, {
+                        id: 6,
+                        label: '二级 2-2'
+                    }]
+                }, {
+                    id: 3,
+                    label: '一级 3',
+                    children: [{
+                        id: 7,
+                        label: '二级 3-1'
+                    }, {
+                        id: 8,
+                        label: '二级 3-2'
+                    }]
+                }],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                },
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1517 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1519 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1516 弄',
+                    zip: 200333
+                }]
+            };
+        }
     };
 </script>
 <style scoped>
