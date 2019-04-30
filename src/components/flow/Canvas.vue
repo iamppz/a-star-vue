422c390167e0d2979fb56c2c664710ee4a6b7a3d
izsl <iamppz@hotmail.com>
2019-04-30 11:18:16 +0800

    Pathing

--- src/components/flow/Canvas.vue
+++ src/components/flow/Canvas.vue
@@ -17,6 +17,8 @@
 <script>
     import processService from "../../service/processService";
     import Start from "./Start";
+    import _ from "lodash";
+
 
     export default {
         components: {Start},
@@ -25,6 +27,8 @@
             if (resp.data.success) {
                 this.nodes = resp.data.data;
             }
+
+            this.pathing(this.startNode);
         },
         data() {
             return {
@@ -34,6 +38,38 @@
         computed: {
             startNode() {
                 return this.nodes.find(node => node.state === 'start');
+            }
+        },
+        methods: {
+            pathing(node) {
+                let func = (paths, basePath, transition) => {
+                    basePath.push(transition.to);
+                    // 基础路径
+                    const basePathMirror = JSON.parse(JSON.stringify(basePath));
+                    if (transition.to.transitions.length > 0) {
+                        for (let i = 0; i < transition.to.transitions.length; i++) {
+                            let next = transition.to.transitions[i];
+                            let currentPath;
+                            if (i > 0) {
+                                // 从第二条路径起，需要重新克隆基础路径，并添加进路径结果集
+                                currentPath = JSON.parse(JSON.stringify(basePathMirror));
+                                paths.push(currentPath);
+                            } else {
+                                currentPath = basePath;
+                            }
+                            func(paths, currentPath, next);
+                        }
+                    }
+                };
+
+                const result = node.transitions.map(transition => {
+                    let path = [transition.from];
+                    let paths = [path];
+                    func(paths, path, transition);
+                    return paths;
+                });
+                console.log(_.flatten(result).map(path => path.map(node => node.id)));
+                return _.flatten(result);
             }
         }
     }
