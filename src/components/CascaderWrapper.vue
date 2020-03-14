a194ccd6e4dcef369d8810fd1a43c993165b0ebd
izsl <iamppz@hotmail.com>
2020-03-14 23:59:20 +0800

    feat(cascader): wrapper for non-array value

---
+++ src/components/CascaderWrapper.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>CascaderWrapper</span>
+</template>
+
+<script>
+  export default {
+    name: 'CascaderWrapper',
+    props: {
+      value: {
+        type: Number,
+        default: null
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
