64957f93f1e6797ff57b4411459c1c615d23ed59
izsl <iamppz@hotmail.com>
2020-03-17 18:18:06 +0800

    feat(form designer): drag

--- src/components/form/DynamicFormDesigner.vue
+++ src/components/form/DynamicFormDesigner.vue
@@ -1,65 +1,37 @@
 <template>
-    <table class="layout">
+    <table class="layout" @mouseup="draggingInfo.target = null"
+           @mouseleave="draggingInfo.target = null" @mousemove="handleMouseMove($event)">
         <tr>
             <td class="left">
-                <p>基础字段</p>
-                <table class="grid">
-                    <tr>
-                        <td>
-                            <span class="iconfont iconinput"></span>
-                            输入框
-                        </td>
-                        <td>
-                            <span class="iconfont icondropdown"></span>
-                            下拉选择
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <span class="iconfont icondatetime"></span>
-                            日期时间
-                        </td>
-                        <td>
-                            <span class="iconfont icontree"></span>
-                            级联选择
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <span class="iconfont iconcheckbox"></span>
-                            多选
-                        </td>
-                        <td>
-                            <span class="iconfont iconradio"></span>
-                            单选
-                        </td>
-                    </tr>
-                </table>
-                <p>高级字段</p>
-                <table class="grid">
-                    <tr>
-                        <td>
-                            <span class="iconfont iconinput"></span>
-                            布局
-                        </td>
-                        <td>
-                            <span class="iconfont icondropdown"></span>
-                            列表
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <span class="iconfont icondatetime"></span>
-                            标签页
-                        </td>
-                        <td>
-                            <span class="iconfont icondropdown"></span>
-                            分隔线
-                        </td>
-                    </tr>
-                </table>
+                <div>基础字段</div>
+                <ul class="panel">
+                    <li class="widget" @mousedown="handleWidgetMouseDown(item, $event)"
+                        v-for="item in basicWidgets" :key="'widget' + item.type">
+                        <span :class="['iconfont', item.icon]"></span>
+                        {{item.name}}
+                    </li>
+                </ul>
+                <div>高级字段</div>
+                <ul class="panel">
+                    <li class="widget" @mousedown="handleWidgetMouseDown(item, $event)"
+                        v-for="item in advancedWidgets" :key="'widget' + item.type">
+                        <span :class="['iconfont', item.icon]"></span>
+                        {{item.name}}
+                    </li>
+                </ul>
+                <div class="widget" v-if="draggingInfo.target !== null"
+                     :style="{top: draggingInfo.y + 'px', left: draggingInfo.x + 'px', position: 'fixed'}">
+                    <span :class="['iconfont', draggingInfo.target.icon]"></span>
+                    {{draggingInfo.target.name}}
+                </div>
             </td>
-            <td class="center">Center</td>
+            <td class="center">
+                <div class="content">
+                    <div class="placeholder">
+                        从左侧拖拽或点击来添加字段
+                    </div>
+                </div>
+            </td>
             <td class="right">Right</td>
         </tr>
     </table>
@@ -68,6 +40,44 @@
 <script>
   export default {
     name: 'DynamicFormDesigner',
+    data() {
+      return {
+        draggingInfo: {
+          target: null,
+          offsetX: null,
+          offsetY: null,
+          x: null,
+          y: null,
+        },
+        basicWidgets: [
+          {type: 'input', icon: 'iconinput', name: '输入框'},
+          {type: 'dropdown', icon: 'icondropdown', name: '下拉选择'},
+          {type: 'datetime', icon: 'icondatetime', name: '日期时间'},
+          {type: 'tree', icon: 'icontree', name: '级联选择'},
+          {type: 'checkbox', icon: 'iconcheckbox', name: '多选'},
+          {type: 'radio', icon: 'iconradio', name: '单选'},
+        ],
+        advancedWidgets: [
+          {type: 'layout', icon: 'iconinput', name: '布局'},
+          {type: 'list', icon: 'iconinput', name: '列表'},
+          {type: 'tab', icon: 'iconinput', name: '标签页'},
+          {type: 'separator', icon: 'iconinput', name: '分割线'},
+        ],
+      };
+    },
+    methods: {
+      handleWidgetMouseDown(widget, event) {
+        this.draggingInfo.target = widget;
+        this.draggingInfo.offsetX = event.offsetX;
+        this.draggingInfo.offsetY = event.offsetY;
+        this.draggingInfo.x = event.clientX - this.draggingInfo.offsetX;
+        this.draggingInfo.y = event.clientY - this.draggingInfo.offsetY;
+      },
+      handleMouseMove(event) {
+        this.draggingInfo.x = event.clientX - this.draggingInfo.offsetX;
+        this.draggingInfo.y = event.clientY - this.draggingInfo.offsetY;
+      },
+    },
   };
 </script>
 
@@ -75,6 +85,11 @@
     .layout {
         width: 100%;
         height: 100%;
+    }
+
+    .layout td {
+        border: 1px solid #dadce0;
+        padding: 10px;
     }
 
     .left {
@@ -86,7 +101,35 @@
         width: 280px;
     }
 
-    .grid {
+    .panel {
         width: 100%;
+        list-style: none;
+        padding: 0;
+    }
+
+    .widget {
+        width: 125px;
+        display: inline-block;
+        cursor: move;
+        border: 1px solid #dadce0;
+        padding: 5px 10px;
+        margin: 2px;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    .content {
+        border: 1px dashed #dadce0;
+        height: 100%;
+        position: relative;
+    }
+
+    .placeholder {
+        font-size: 20px;
+        position: absolute;
+        top: calc(50% - 15px);
+        left: calc(50% - 130px);
     }
 </style>
